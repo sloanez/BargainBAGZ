32665bfe7fa3a57d837325049e625db243ffae7f
sloanez <zjs5048@gmail.com>
2014-04-29 01:07:38 -0400

    Categories Tree Fully Implemented
    Depth, man. Depth.

---
+++ categories/php/individual-signup.php
@@ -0,0 +1,18 @@
+<?php
+$con=mysqli_connect("localhost","zjs5048","rt74937LT","infiniteloops");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+$result = mysqli_query($con,"SELECT * FROM Users");
+
+while($row = mysqli_fetch_array($result))
+  {
+  	echo $row['Name'] . " " . $row['Email'];
+  	echo "<br>";
+  }
+
+mysqli_close($con);
+?>
