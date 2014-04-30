3cb113d9754dc39330e204ba97b5986178d2b8dd
sloanez <zjs5048@gmail.com>
2014-04-29 23:49:48 -0400

    jewelry done

--- categories/necklaces-view.php
+++ categories/necklaces-view.php
@@ -33,6 +33,33 @@
 				<h2	class="header_text">Necklaces</h2>
 			</div>
 			<!--End of Page Title-->
+			
+			<!-- All Necklaces -->
+
+			<?php
+				$con=mysqli_connect("127.0.0.1","loops","password","infiniteloops");
+			
+				if (mysqli_connect_errno())
+				{
+					echo "Failed to connect to MySQL: " . mysqli_connect_error();
+				}
+
+				$result = mysqli_query($con,"SELECT * FROM `Items` WHERE Category in ('Necklaces')");
+
+				if ($result && mysqli_num_rows($result) > 0) {
+					echo "<div id=\"nav\" class=\"dynamic_size\">
+							<h3 class=\"dynamic_text\"></h3>
+							<div id=\"list\">
+							<ul class=\"dynamic\" data-role=\"listview\" data-inset=\"true\" data-theme=\"a\">";
+					while($row = mysqli_fetch_array($result)){
+						echo "<li><a href=\"../itemPage.php?itemId=" . $row["Item_No"] . "\" rel=\"external\">" . $row['Item_Name'] . "</a></li>";
+					}
+					echo "</ul></div></div>";
+				}
+				else {
+					echo "<h1>Sorry, no items exist.</h1>";
+				}
+			?>
 		</div>
 	</body>
 </html>
