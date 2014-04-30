c605524ba21282ef8743b4dbc234a5417eb63794
sloanez <zjs5048@gmail.com>
2014-04-30 16:06:44 -0400

    individual signup

--- php/sell.php
+++ php/sell.php
@@ -1,23 +1,27 @@
 <?php
-
-
-// THIS WAS NOT IMPLEMENTED YET
-
-
-$con=mysqli_connect("localhost","zjs5048","rt74937LT","infiniteloops");
+$con=mysqli_connect("127.0.0.1","loops","password","infiniteloops");
 // Check connection
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 
-$result = mysqli_query($con,"SELECT * FROM Users");
+$email = $_POST["email"];
+$password = $_POST["password"];
+$f_name = $_POST["fname"];
+$l_name = $_POST["lname"];
+$age = $_POST["age"];
+$gender = $_POST["gender"];
+$phone = $_POST["phone"];
+$addr = $_POST["addr_1"]." ".$_POST["addr_2"];
+$city = $_POST["city"];
+$zip = $_POST["zip"];
+$state = $_POST["state"];
 
-while($row = mysqli_fetch_array($result))
-  {
-  	echo $row['Name'] . " " . $row['Email'];
-  	echo "<br>";
-  }
+
+$result = mysqli_query($con,"INSERT Into Users (Email,Password,Phone_Num) Values ('$email', '$password', '$phone')");
+
+echo mysqli_error($con);
 
 mysqli_close($con);
 ?>
