599436945685659a30dd24be91c131eb58447045
Grace Lee <gracelee.0608@gmail.com>
2014-04-29 21:54:18 -0400

    Item Page DONE
    Just need to add functionality to Bidding and Buy It Now

--- itemPage.php
+++ itemPage.php
@@ -22,7 +22,7 @@
 					<a href="index.html" data-ajax="false" data-prefetch data-role="button" id="homebtn" class="home_button">BACK</a>
 				</div>
 				<div id="header_home">
-				<a href="index.html" data-ajax="false" data-prefetch data-role="button" id="homebtn" class="home_button">Home</a>
+				<a href="index.html" data-ajax="false" data-prefetch data-role="button" id="homebtn" class="home_button">HOME</a>
 				</div>	
 			</div>	
 			<!-- End of Header Section -->
@@ -40,40 +40,69 @@
 				echo "Failed to connect to MySQL: " . mysqli_connect_error();
 			}
 			
-			$itemId = $_GET["itemId"];
+
+			if (count($_GET) > 0) {
+				$itemId = $_GET["itemId"];
+			}
+			else {
+				$itemId = -1;
+			}
 
 			$result = mysqli_query($con,"SELECT * FROM `Items` WHERE Item_No = $itemId");
 
 			if ($result && mysqli_num_rows($result) > 0) {
 				while($row = mysqli_fetch_array($result)){
+					// Item Name
 					echo "<h1>" . $row['Item_Name'] . "</h1>";
+					
 					// Seller: MAKE IT A LINK
-					echo "<h3><i>" . $row['Description'] . "</i></h3>";
-					// For more info: description (if url exists)
-					// Shipped From: GET FROM ADDR OF SELLER ID IN addresstable
+					echo "Seller ID: <a href=\"index.html\">" . $row['Seller_Id'] . "</a>";
+
+
+					// Item Description 
+					echo "<br><br><b><i>" . $row['Description'] . "</i></b>";
+					
+					if ($row['URL']) {
+						echo "<br><br>Click <a href=\"" . $row['URL'] . "\"> here</a> for more information about this item.";
+					}
+					
+					// Shipped From State
+					$seller = $row['Seller_Id'];
+					
+					$state = mysqli_query($con,"SELECT * FROM `Address` WHERE User_Id = $seller");
+
+					$newRow = mysqli_fetch_array($state);
+
+					echo "<br><br><b>Shipped from</b>: " . $newRow['Stayt'];
+
+					echo "<br><br><hr>";
+
+					
+					// Current Highest Bid
 					echo "<br><br><b>Highest Bid</b>: $" . $row['Highest_Bid'];
 
+					// Countdown to end of auction 
 					$end = strtotime($row['End_Auction']);
 
-					$today = time();
+					$now = time();
 
-					echo "<br><br>" . $end;
-					echo "<br><br>" . $today;
+					$diff = $end - $now;
 
-					$diff = $end - $today;
+					$days = floor($diff / 60 / 60 / 24);
+					$hours = floor(($diff-($days*60*60*24))/60/60);
+					$mins = floor(($diff-($days*60*60*24)-($hours*60*60))/60);
+					$secs = floor(($diff-($days*60*60*24)-($hours*60*60))-($mins *60));
 
-					$timeLeft = date('d-s');
-
-					//echo "<br><br>" . $timeLeft;
-
-					echo "<br><br><b>Time left until end of auction</b>: " . $timeLeft;
+					echo "<br><br><b>Time left until end of auction</b>: " . $days . " days, " . $hours . " hours, " . $mins . " minutes, and " . $secs . " seconds.";;
 
 
-
+					// Buy it now
 					echo "<br><br><b>Buy It Now Price</b>: $" .  $row['Buy_Now_Price'] . "<form name=\"buyNow\" method=\"post\" action=\"buynow.php\">
 					<input type=\"submit\" value=\"Buy It Now!\">
 					</form>";
 
+
+					// Bid for item
 					$nextBid = ($row['Highest_Bid'] * .05) + $row['Highest_Bid'];
 
 					echo "<br><br><form name=\"bid\" method=\"post\" action=\"bid.php\">
